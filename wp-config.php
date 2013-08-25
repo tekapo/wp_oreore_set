222a18abefa387e19369e3270165dfb2657a9b05
JOTAKI Taisuke <tekapo@gmail.com>
2013-08-26 07:26:19 +0900

    test.

--- wp-config.php
+++ wp-config.php
@@ -81,7 +81,7 @@ define('WPLANG', 'ja');
  * この値を true にすると、開発中に注意 (notice) を表示します。
  * テーマおよびプラグインの開発者には、その開発環境においてこの WP_DEBUG を使用することを強く推奨します。
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* 編集が必要なのはここまでです ! WordPress でブログをお楽しみください。 */
 
